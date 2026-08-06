832a50c0b7b1669dfa4a7beb30c9f3630da1eaef
CL34RBOX <lazyboii147@gmail.com>
2026-08-06 13:16:39 -0700

    Add DomHandler class with HTML sanitization method

---
+++ src/handlers/domHandler.ts
@@ -0,0 +1,10 @@
+export class DomHandler {
+    public sanitizeHtml(input: string): string {
+        return input
+            .replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;")
+            .replace(/"/g, "&quot;")
+            .replace(/'/g, "&#x27;");
+    }
+}
